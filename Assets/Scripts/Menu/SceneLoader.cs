137a07b3b354f5eddb82e468c922c1602aaa76ba
Techcrafter <skyboyskyman@gmail.com>
2020-01-03 11:57:14 +0100

    Added basic menu.

---
+++ Assets/Scripts/Menu/SceneLoader.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneLoader : MonoBehaviour {
+	
+	private GameObject LoadingText;
+	
+	// Start is called before the first frame update
+    void Start()
+    {
+        LoadingText = GameObject.Find("LoadingText");
+		LoadingText.SetActive(false);
+    }
+	
+	public void loadScene(int level)
+	{
+		LoadingText.SetActive(true);
+		Application.LoadLevel(level);
+	}
+	
+	public void quitGame()
+	{
+		LoadingText.SetActive(true);
+		Application.Quit();
+	}
+}
