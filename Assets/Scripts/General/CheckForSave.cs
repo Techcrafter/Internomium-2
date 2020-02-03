efbd5d4039253d22c28281c129c5e679d7670eab
Techcrafter <skyboyskyman@gmail.com>
2020-02-03 15:38:54 +0100

    Version 0.0.2.1: Fixed bug in save system and updated project to newest Unity version (2019.3.0f6).

--- Assets/Scripts/General/CheckForSave.cs
+++ Assets/Scripts/General/CheckForSave.cs
@@ -7,7 +7,7 @@ public class CheckForSave : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        if(PlayerPrefs.GetFloat("MovementSpeed") == 0 || PlayerPrefs.GetFloat("RotationSpeed") == 0 || PlayerPrefs.GetFloat("Missile") == 0 || PlayerPrefs.GetFloat("Level") == 0)
+        if(PlayerPrefs.GetFloat("MovementSpeed") == 0 || PlayerPrefs.GetFloat("RotationSpeed") == 0 || PlayerPrefs.GetFloat("MissilesLevel") == 0 || PlayerPrefs.GetFloat("Level") == 0)
 		{
 			PlayerPrefs.SetFloat("MovementSpeed", 1);
 			PlayerPrefs.SetFloat("RotationSpeed", 1);
